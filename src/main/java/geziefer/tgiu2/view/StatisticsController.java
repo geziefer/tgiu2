ed7fd019debb6e3aa24feca7a5026a9d19c22b2d
Alexander Ruehl <alexander.ruehl@syrocon.de>
2016-10-11 23:15:27 +0200

    implement a few statistic charts

--- src/main/java/geziefer/tgiu2/view/StatisticsController.java
+++ src/main/java/geziefer/tgiu2/view/StatisticsController.java
@@ -5,16 +5,24 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.PropertyResourceBundle;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.HorizontalBarChartModel;
+import org.primefaces.model.chart.LegendPlacement;
+import org.primefaces.model.chart.PieChartModel;
+
 import geziefer.tgiu2.LocalEntityManagerFactory;
+import geziefer.tgiu2.entity.Player;
 import geziefer.tgiu2.entity.Rank;
 
 @Named
@@ -22,7 +30,12 @@ import geziefer.tgiu2.entity.Rank;
 public class StatisticsController implements Serializable {
 	private static final long serialVersionUID = -270705952015361605L;
 
+	@Inject
+	private transient PropertyResourceBundle msg;
+
 	private List<Rank> ranks = new ArrayList<>();
+
+	private List<Player> players = new ArrayList<>();
 
 	private Long roundCount;
 
@@ -31,16 +44,91 @@ public class StatisticsController implements Serializable {
 		// TODO: depending on combobox selection
 		LocalDate from = LocalDate.now().withDayOfYear(1);
 		LocalDate to = from.plusYears(1).minusDays(1);
-		TypedQuery<Rank> query = em.createNamedQuery("Rank.findByDate", Rank.class);
-		query.setParameter("from", from);
-		query.setParameter("to", to);
-		ranks = query.getResultList();
+		TypedQuery<Rank> query1 = em.createNamedQuery("Rank.findByDate", Rank.class);
+		query1.setParameter("from", from);
+		query1.setParameter("to", to);
+		ranks = query1.getResultList();
+
+		TypedQuery<Player> query2 = em.createNamedQuery("Player.findAll", Player.class);
+		players = query2.getResultList();
 
 		roundCount = ranks.stream().filter(distinctByKey(r -> r.getRound().getId())).count();
 	}
 
-	public List<Rank> getRoundsPerPlayer() {
-		return null;
+	public HorizontalBarChartModel getRounds() {
+		HorizontalBarChartModel model = new HorizontalBarChartModel();
+		model.setTitle(msg.getString("statistics.chart.rounds.title"));
+		model.setDatatipFormat("%d");
+		ChartSeries series = new ChartSeries();
+		for (int i = players.size() - 1; i >= 0; i--) {
+			Player player = players.get(i);
+			Long count = ranks.stream().filter(r -> r.getPlayer().getName().equals(player.getName())).count();
+			series.set(player.getName(), count);
+		}
+		series.set(msg.getString("statistics.chart.rounds.total"), roundCount);
+		model.addSeries(series);
+
+		return model;
+	}
+
+	public PieChartModel getFirstRanks() {
+		PieChartModel model = new PieChartModel();
+		model.setTitle(msg.getString("statistics.chart.firstranks.title"));
+		model.setLegendPosition("w");
+		model.setLegendPlacement(LegendPlacement.OUTSIDEGRID);
+		model.setShowDataLabels(true);
+		for (Player player : players) {
+			Long count = ranks.stream().filter(r -> r.getPlayer().getName().equals(player.getName()))
+					.filter(r -> (r.getRank() == 1)).count();
+			model.set(player.getName(), count);
+		}
+
+		return model;
+	}
+
+	public PieChartModel getSecondRanks() {
+		PieChartModel model = new PieChartModel();
+		model.setTitle(msg.getString("statistics.chart.secondranks.title"));
+		model.setLegendPosition("w");
+		model.setLegendPlacement(LegendPlacement.OUTSIDEGRID);
+		model.setShowDataLabels(true);
+		for (Player player : players) {
+			Long count = ranks.stream().filter(r -> r.getPlayer().getName().equals(player.getName()))
+					.filter(r -> (r.getRank() == 2)).count();
+			model.set(player.getName(), count);
+		}
+
+		return model;
+	}
+
+	public PieChartModel getThirdRanks() {
+		PieChartModel model = new PieChartModel();
+		model.setTitle(msg.getString("statistics.chart.thirdranks.title"));
+		model.setLegendPosition("w");
+		model.setLegendPlacement(LegendPlacement.OUTSIDEGRID);
+		model.setShowDataLabels(true);
+		for (Player player : players) {
+			Long count = ranks.stream().filter(r -> r.getPlayer().getName().equals(player.getName()))
+					.filter(r -> (r.getRank() == 3)).count();
+			model.set(player.getName(), count);
+		}
+
+		return model;
+	}
+
+	public PieChartModel getDNFRanks() {
+		PieChartModel model = new PieChartModel();
+		model.setTitle(msg.getString("statistics.chart.dnfranks.title"));
+		model.setLegendPosition("w");
+		model.setLegendPlacement(LegendPlacement.OUTSIDEGRID);
+		model.setShowDataLabels(true);
+		for (Player player : players) {
+			Long count = ranks.stream().filter(r -> r.getPlayer().getName().equals(player.getName()))
+					.filter(r -> (r.getRank() > 3)).count();
+			model.set(player.getName(), count);
+		}
+
+		return model;
 	}
 
 	private <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
